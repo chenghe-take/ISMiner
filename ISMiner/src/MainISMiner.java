e631f366cbc6f70827af2017435ebd3859bb1a0d
chenghe-take <157938681+chenghe-take@users.noreply.github.com>
2024-06-21 20:04:15 +0800

    Update MainISMiner.java

--- ISMiner/src/MainISMiner.java
+++ ISMiner/src/MainISMiner.java
@@ -2,7 +2,7 @@ import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.URL;
 
-public class MainTestTSeqMiner1 {
+public class MainTestISMiner {
 
 	public static void main(String [] arg) throws IOException, ClassNotFoundException{
 
